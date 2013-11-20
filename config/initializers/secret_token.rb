3110d057918ecbc8fcc54b68199537bd04caf85c
kang cao <cooker_bj@hotmail.com>
2013-11-20 08:51:24 +0800

    initilize

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Ithelp::Application.config.secret_key_base = '2a634a2a4eb36830db86544ded8ee6471c1b208b29c76a31720f028ea66a69488f46292ea9db3f02470129270c478e8da0a2632a1f2365f760b7a4c8bb0ff599'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Ithelp::Application.config.session_store :cookie_store, key: '_ithelp_session'
